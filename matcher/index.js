d80ec66517f2ad3e8358e626893e431fcfe0df74
DIWAN ANUJ JITENDRA <anujdiwan2008@gmail.com>
2018-12-07 15:55:37 +0530

    Wrote the pattern matcher.

--- matcher/index.js
+++ matcher/index.js
@@ -1,2 +1,18 @@
 'use strict';
-const patternDict=require('../patterns');
+const patterns=require('../patterns');
+const XRegExp = require('xregexp');
+
+let matchPattern=(str,cb) => {
+  let getResult=patterns.find((item) => {
+      return XRegExp.test(str, XRegExp(item.pattern, "ig"));
+  });
+  if(getResult){
+      return cb({
+          intent: getResult.intent
+      });
+  }else{
+      return cb({
+
+      });
+  }
+};
