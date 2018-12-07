94db48aad14726730d9879eb6f56cc86671f2580
DIWAN ANUJ JITENDRA <anujdiwan2008@gmail.com>
2018-12-07 17:09:46 +0530

    Querying Yahoo API is complete. Used Promise etc.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const rl=readLine.createInterface({
    terminal:false,
 });
 const matcher=require('./matcher');
-
+const weather=require('./weather');
 rl.setPrompt('> ');
 rl.prompt();
 rl.on('line',(reply)=>{
@@ -14,6 +14,7 @@ rl.on('line',(reply)=>{
         switch(data.intent){
             case 'Hello':
                 console.log(`${data.entities.greeting} to you too!`);
+                rl.prompt();
                 break;
             case 'Exit':
                 console.log("Have a great day!");
@@ -21,10 +22,19 @@ rl.on('line',(reply)=>{
                 break;
             case 'CurrentWeather':
                 console.log(`Checking weather for ${data.entities.city}.`);
+                weather(data.entities.city,'current')
+                    .then(response=>{
+                        console.log(response);
+                        rl.prompt();
+                    })
+                    .catch(error=>{
+                       console.log("There seems to be a problem connecting to the Weather service.");
+                        rl.prompt();
+                    });
                 break;
             default:
                 console.log("I don't know what you mean :(");
+                rl.prompt();
         }
     });
-    rl.prompt();
 });
