4bda6370fb3b5c017db5c44186ee0bdde0dca40c
DIWAN ANUJ JITENDRA <anujdiwan2008@gmail.com>
2018-12-07 16:04:26 +0530

    Handled some simple patterns.

--- app.js
+++ app.js
@@ -5,9 +5,23 @@ const rl=readLine.createInterface({
    output:process.stdout,
    terminal:false,
 });
+const matcher=require('./matcher');
+
 rl.setPrompt('> ');
 rl.prompt();
 rl.on('line',(reply)=>{
-    console.log(`You said ${reply}`);
+    matcher(reply,data =>{
+        switch(data.intent){
+            case 'Hello':
+                console.log("A big hello from Vanilla!");
+                break;
+            case 'Exit':
+                console.log("Have a great day!");
+                process.exit(0);
+                break;
+            default:
+                console.log("I don't know what you mean :(");
+        }
+    });
     rl.prompt();
 });
--- matcher/index.js
+++ matcher/index.js
@@ -2,17 +2,18 @@
 const patterns=require('../patterns');
 const XRegExp = require('xregexp');
 
-let matchPattern=(str,cb) => {
-  let getResult=patterns.find((item) => {
-      return XRegExp.test(str, XRegExp(item.pattern, "ig"));
-  });
-  if(getResult){
-      return cb({
-          intent: getResult.intent
-      });
-  }else{
-      return cb({
-
-      });
-  }
+let matchPattern = (str, cb) => {
+    let getResult = patterns.find((item) => {
+        return XRegExp.test(str, XRegExp(item.pattern, "ig"));
+    });
+    if (getResult) {
+        return cb({
+            intent: getResult.intent
+        });
+    } else {
+        return cb({
+            intent: 'Irrelevant',
+        });
+    }
 };
+module.exports=matchPattern;
